946f4ab0897b2a41a932e5b79f721227a9e59e59
Davjd <k.davjd@gmail.com>
2022-05-16 21:47:26 +0300

    add predict_by_recursion() fcn

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -2,8 +2,10 @@
 Компьютер сам загадывает и сам угадывает число
 """
 
+from itertools import count
 import numpy as np
 
+count
 
 def random_predict(number: int = 1) -> int:
     """Рандомно угадываем число
@@ -44,7 +46,7 @@ def score_game(random_predict) -> int:
     print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
     return score
 
-def predict_by_divide(number: int =1, min_number: int=1, max_number: int=100) -> int:
+def predict_by_divide(number: int =1, min_number: int=1, max_number: int=100, count: int=0) -> int:
     """Функция делит исследуемую область на равные интервалы в две единицы, отбрасывая 
     хвосты. 
 
@@ -52,55 +54,120 @@ def predict_by_divide(number: int =1, min_number: int=1, max_number: int=100) ->
         number (int, optional): Загаданное число. Defaults to 1.
         min_number (int, optional): Нижняя граница числа. Defaults to 1.
         max_number (int, optional): Верхняя граница числа. Defaults to 100.
+        count (int, optional): Верхняя граница числа. Defaults to 0.
 
     Returns:
         int: Число попыток.
     """
-    count=0
+    
     
     divide_list = [border for border in range(min_number,max_number) if \
         border%2 == 0]
     
     if min(divide_list) < number < max(divide_list): #Начало и конец строго отбрасываются.
         for border in divide_list:
+            count += 1
             if border > number: #Поскольку шаг два, то число либо предыдущее, либо то что за ним.
-                if number == border-2:
+                if number == border-1:
                     count += 1
                     break                                   
-                elif number == border-1:
-                        count+=1
-                        break
+                elif number == border-2:
+                    count += 2
+                    break
                 else:
-                    raise Exception('Поздравляю, что-то не так.')
+                    raise Exception('Ошибка в исследуемом диапазоне')
             
             
     elif max_number >= number >= max(divide_list): #Ищем число в верхнем хвосте
-        if number == max(divide_list)+2:
+        if number == max(divide_list):
             count += 1           
         elif number == max(divide_list)+1:
-            count += 1
-        elif number == max(divide_list):
-            count += 1       
+            count += 2
+        elif number == max(divide_list)+2:
+            count += 3       
         else:
             raise Exception('Ошибка в верхних граничных исключениях')
     
     elif min_number <= number <= min(divide_list): #Ищем число в нижнем хвосте
-        if number == min(divide_list)-2:
+        if number == min(divide_list):
             count += 1                
         elif number == min(divide_list)-1:
-            count += 1
-        elif number == min(divide_list):
-            count += 1
+            count += 2
+        elif number == min(divide_list)-2:
+            count += 3
         else:
             raise Exception('Ошибка в нижних граничных исключениях')
         
     
     return count
  
+
+def predict_by_recursion(number, min_number: int=1, max_number: int=100) -> predict_by_divide():
+    
+    """Функция сужает исследуемую область для функции predict_by_divide()
+
+    Args:
+        number (int, optional): _description_. Defaults to 1.
+        min_number (int, optional): _description_. Defaults to 1.
+        max_number (int, optional): _description_. Defaults to 100.
+
+    Returns:
+        int: predict_by_divide()
+    """
+    max_number
+    min_number
+    count = 0
+    
+    def max_border_near(number: int):  
+        nonlocal count
+        nonlocal max_number
+        if number < int(np.average([min_number,max_number])):
+            if max_number - int(np.average([min_number,max_number])) < min_number:
+                return max_number
+            max_number -= int(np.average([min_number,max_number]))
+            count += 1
+            return max_border_near(number)
+        return max_number
+    
+    def min_border_near(number: int):
+        nonlocal count
+        nonlocal min_number
+        average = (int(np.average([min_number,max_number])))
+        if number > average:
+            if count > 10: 
+                #В приложенном файле прзентации для юпитера я попытался показать
+                #почему тут такое число, но не справился. Нашел его руками.               
+                return min_number
+            
+            next_step = (average + min_number)/2
+            if  next_step > max_number:
+                return min_number
+            min_number = next_step
+            count += 1
+            return min_border_near(number)
         
+        return min_number
+    
+    if number == min_number: #Исключение, которое не нашел как элегантно обойти.
+        count += 1
+        return count
+    
+    elif number < int(np.average([min_number,max_number])): #Мне показалось что есть зависимость между началом отсечений.
+        count += 1
+        max_border_near(number)
+        min_border_near(number)
+    else:
+        count += 1
+        min_border_near(number)
+        max_border_near(number)
+    return predict_by_divide(number, int(min_number), int(max_number), count)
+        
+
+    
 if __name__ == "__main__":
     # RUN
     score_game(random_predict)
     score_game(predict_by_divide)
+    score_game(predict_by_recursion)
     
     
